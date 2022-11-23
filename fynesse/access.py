2564f2ebeec462b1c024cc94bb556db873357db9
Marcus Handley <printlnHi@users.noreply.github.com>
2022-11-23 00:13:54 +0000

    added load_file as first experiment with moving to fynesse

--- fynesse/access.py
+++ fynesse/access.py
@@ -1,17 +1,21 @@
 from .config import *
 
-"""These are the types of import we might expect in this file
-import httplib2
-import oauth2
-import tables
-import mongodb
-import sqlite"""
+import pymysql
 
 # This file accesses the data
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
 
-def data():
-    """Read the data from the web or local file, returning structured format such as a data frame"""
-    raise NotImplementedError
+#TODO: Rename 
 
+def load_file(conn,table,file):
+    """
+    Lode local data file into table
+    :param conn: the Connection object
+    :param table: the table to query
+    :param file: the local file to load from
+    """
+    command = (f"LOAD DATA LOCAL INFILE '{file}' INTO TABLE {table} FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES STARTING BY '' TERMINATED BY '\\n'")
+    cur = conn.cursor()
+    cur.execute(command)
+    return cur.fetchall()
