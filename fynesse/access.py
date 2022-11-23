4764660bcd54fd946df25dbecad2e14f0d6c56e0
Marcus Handley <printlnHi@users.noreply.github.com>
2022-11-23 00:39:01 +0000

    moving part of Q1.B to fynesse

--- fynesse/access.py
+++ fynesse/access.py
@@ -6,8 +6,86 @@ import pymysql
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
 
-#TODO: Rename 
+def create_connection(user, password, host, database, port=3306):
+    """ Create a database connection to the MariaDB database
+        specified by the host url and database name.
+    :param user: username
+    :param password: password
+    :param host: host url
+    :param database: database
+    :param port: port number
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = pymysql.connect(user=user,
+                               passwd=password,
+                               host=host,
+                               port=port,
+                               local_infile=1,
+                               db=database
+                               )
+    except Exception as e:
+        print(f"Error connecting to the MariaDB Server: {e}")
+    return conn
 
+
+def create_database_ifne(conn):
+    cur = conn.cursor()
+    cur.execute("CREATE DATABASE IF NOT EXISTS `property_prices` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin")
+    return cur.fetchall()
+
+def create_pricepaid_table(conn):
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS `pp_data`")
+    cur.execute("""CREATE TABLE IF NOT EXISTS `pp_data` (
+    `transaction_unique_identifier` tinytext COLLATE utf8_bin NOT NULL,
+    `price` int(10) unsigned NOT NULL,
+    `date_of_transfer` date NOT NULL,
+    `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
+    `property_type` varchar(1) COLLATE utf8_bin NOT NULL,
+    `new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,
+    `tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,
+    `primary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
+    `secondary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
+    `street` tinytext COLLATE utf8_bin NOT NULL,
+    `locality` tinytext COLLATE utf8_bin NOT NULL,
+    `town_city` tinytext COLLATE utf8_bin NOT NULL,
+    `district` tinytext COLLATE utf8_bin NOT NULL,
+    `county` tinytext COLLATE utf8_bin NOT NULL,
+    `ppd_category_type` varchar(2) COLLATE utf8_bin NOT NULL,
+    `record_status` varchar(2) COLLATE utf8_bin NOT NULL,
+    `db_id` bigint(20) unsigned NOT NULL
+    ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1""")
+    cur.execute("ALTER TABLE `pp_data` ADD PRIMARY KEY (`db_id`), MODIFY `db_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1")
+    return cur.fetchall()
+
+def add_pricepaid_indicies(conn):
+    cur = conn.cursor()
+    cur.execute("CREATE INDEX `pp.postcode` USING HASH ON `pp_data` (postcode)");
+    cur.execute("CREATE INDEX `pp.date` USING HASH ON `pp_data` (date_of_transfer)")
+    return cur.fetchall()
+
+
+def select_top(conn, table,  n):
+    """
+    Query n first rows of the table
+    :param conn: the Connection object
+    :param table: The table to query
+    :param n: Number of rows to query
+    """
+    cur = conn.cursor()
+    cur.execute(f'SELECT * FROM {table} LIMIT {n}')
+
+    rows = cur.fetchall()
+    return rows
+
+def head(conn, table, n=5):
+  rows = select_top(conn, table, n)
+  for r in rows:
+      print(r)
+
+#TODO: Rename 
 def load_file(conn,table,file):
     """
     Lode local data file into table
